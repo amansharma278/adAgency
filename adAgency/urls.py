c48d795f764f2d4e1841d53b99cd8198b44cdf58
Ram Vijay Sharma <ram.sharma@hexaviewtech.com>
2026-02-19 13:10:08 +0530

    Added jwt login, verify and refresh endpoint

--- adAgency/urls.py
+++ adAgency/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from users.views import profile
 
@@ -20,4 +21,7 @@ urlpatterns = [
     path('api-auth/',include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/', profile),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
